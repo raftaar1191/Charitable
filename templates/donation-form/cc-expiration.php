02e7199c5fce636072f7525143d66b8714b1125c
Eric Daams <eric@164a.com>
2017-06-19 11:33:24 +0930

    Fixed incorrect month field in cc-expiration template

--- templates/donation-form/cc-expiration.php
+++ templates/donation-form/cc-expiration.php
@@ -13,39 +13,38 @@
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-if ( ! isset( $view_args[ 'form' ] ) || ! isset( $view_args[ 'field' ] ) ) {
-    return;
+if ( ! isset( $view_args['form'] ) || ! isset( $view_args['field'] ) ) {
+	return;
 }
 
-$form           = $view_args[ 'form' ];
-$field          = $view_args[ 'field' ];
-$classes        = $view_args[ 'classes' ];
-$is_required    = isset( $field[ 'required' ] ) ? $field[ 'required' ] : false;
+$form           = $view_args['form'];
+$field          = $view_args['field'];
+$classes        = $view_args['classes'];
+$is_required    = isset( $field['required'] ) ? $field['required'] : false;
 $current_year   = date( 'Y' );
 
 ?>
 <div id="charitable_field_<?php echo $field['key'] ?>" class="<?php echo $classes ?>">
-    <?php if ( isset( $field['label'] ) ) : ?>
-        <label for="charitable_field_<?php echo $field['key'] ?>">
-            <?php echo $field['label'] ?>
-            <?php if ( $is_required ) : ?>
-                <abbr class="required" title="required">*</abbr>
-            <?php endif ?>
-        </label>
-    <?php endif ?>    
-    <select name="<?php echo $field['key'] ?>[
-    ]" class="month">
-        <?php foreach ( range( 1, 12 ) as $month ) :
-            $padded_month = sprintf( '%02d', $month );
-            ?>
-            <option value="<?php echo $padded_month ?>"><?php echo $padded_month ?></option>
-        <?php endforeach ?>
-    </select>
-    <select name="<?php echo $field['key'] ?>[year]" class="year">
-        <?php for ( $i = 0; $i < 15; $i++ ) :
-            $year = $current_year + $i;
-            ?>
-            <option value="<?php echo $year ?>"><?php echo $year ?></option>
-        <?php endfor ?>
-    </select>
+	<?php if ( isset( $field['label'] ) ) : ?>
+		<label for="charitable_field_<?php echo $field['key'] ?>">
+			<?php echo $field['label'] ?>
+			<?php if ( $is_required ) : ?>
+				<abbr class="required" title="required">*</abbr>
+			<?php endif ?>
+		</label>
+	<?php endif ?>
+	<select name="<?php echo $field['key'] ?>[month]" class="month">
+		<?php foreach ( range( 1, 12 ) as $month ) :
+			$padded_month = sprintf( '%02d', $month );
+			?>
+			<option value="<?php echo $padded_month ?>"><?php echo $padded_month ?></option>
+		<?php endforeach ?>
+	</select>
+	<select name="<?php echo $field['key'] ?>[year]" class="year">
+		<?php for ( $i = 0; $i < 15; $i++ ) :
+			$year = $current_year + $i;
+			?>
+			<option value="<?php echo $year ?>"><?php echo $year ?></option>
+		<?php endfor ?>
+	</select>
 </div><!-- #charitable_field_<?php echo $field['key'] ?> -->
