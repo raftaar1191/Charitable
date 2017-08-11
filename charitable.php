cf27727b041288cfe9594ce555eff672eea8db84
Eric Daams <eric@164a.com>
2017-08-11 22:09:17 +0930

    1.5.0 - Alpha 7

--- charitable.php
+++ charitable.php
@@ -3,7 +3,7 @@
  * Plugin Name:         Charitable
  * Plugin URI:          https://www.wpcharitable.com
  * Description:         The WordPress fundraising alternative for non-profits, created to help non-profits raise money on their own website.
- * Version:             1.5.0-alpha.6
+ * Version:             1.5.0-alpha.7
  * Author:              WP Charitable
  * Author URI:          https://wpcharitable.com
  * Requires at least:   4.1
@@ -36,7 +36,7 @@ if ( ! class_exists( 'Charitable' ) ) :
 		 *
 		 * @var     string
 		 */
-		const VERSION = '1.5.0-alpha.6';
+		const VERSION = '1.5.0-alpha.7';
 
 		/**
 		 * Version of database schema.
