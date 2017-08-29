d9896d1b200c8e7e4cd02498289b78aa12b6f01e
Eric Daams <eric@164a.com>
2017-08-29 13:26:10 +0930

    Set version of master branch to 1.5.0

--- charitable.php
+++ charitable.php
@@ -3,11 +3,7 @@
  * Plugin Name:         Charitable
  * Plugin URI:          https://www.wpcharitable.com
  * Description:         The WordPress fundraising alternative for non-profits, created to help non-profits raise money on their own website.
-<<<<<<< HEAD
  * Version:             1.5.0
-=======
- * Version:             1.4.20
->>>>>>> stable
  * Author:              WP Charitable
  * Author URI:          https://wpcharitable.com
  * Requires at least:   4.1
