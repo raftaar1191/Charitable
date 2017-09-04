28cedd75e4305317de00fa97fc5510d3550e4af0
Eric Daams <eric@164a.com>
2017-09-04 17:59:19 +0930

    Change button text based on selected option. Hide button unless option is selected.

--- includes/admin/views/metaboxes/donation/donation-actions.php
+++ includes/admin/views/metaboxes/donation/donation-actions.php
@@ -11,10 +11,10 @@ global $post;
 $helper  = charitable_get_donation_actions();
 $actions = $helper->get_actions();
 ?>
-<div id="charitable-donation-actions-metabox-wrapper" class="charitable-metabox">
-	<div id="charitable-donation-actions-form">
+<div id="charitable-donation-actions-metabox-wrapper" class="charitable-metabox charitable-actions-form-wrapper">
+	<div id="charitable-donation-actions-form" class="charitable-actions-form">
 		<?php do_action( 'charitable_donation_actions_start', $post->ID ); ?>
-		<select id="charitable_donation_actions" name="charitable_donation_action">
+		<select id="charitable_donation_actions" name="charitable_donation_action" class="charitable-action-select">
 			<option value=""><?php _e( 'Select an action', 'charitable' ) ?></option>
 			<?php foreach ( $helper->get_groups() as $label => $group_actions ) : ?>
 				<?php if ( ! empty( $label ) ) : ?>
@@ -30,7 +30,7 @@ $actions = $helper->get_actions();
 		</select>
 		<?php do_action( 'charitable_donation_actions_end', $post->ID ); ?>
 	</div><!-- #charitable-donation-actions-form -->
-	<div id="charitable-donation-actions-submit">
+	<div id="charitable-donation-actions-submit" class="charitable-actions-submit">
 		<button type="submit" class="button-primary" title="<?php esc_attr_e( 'Submit', 'charitable' ) ?>"><?php _e( 'Submit', 'charitable' ) ?></button>
 		<div class="clear"></div>
 	</div><!-- #charitble-donation-actions-submit -->
