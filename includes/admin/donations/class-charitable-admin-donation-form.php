531cda00551fc9dd631100e3c0f160b44173d8b2
Eric Daams <eric@164a.com>
2017-09-13 17:23:40 +0930

    Began overhaul of how form fields are rendered, decoupling the view component from the form model.

---
+++ includes/admin/donations/class-charitable-admin-donation-form.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * Admin donation form model class.
+ *
+ * @version   1.5.0
+ * @package   Charitable/Classes/Charitable_Admin_Donation_Form
+ * @author    Eric Daams
+ * @copyright Copyright (c) 2017, Studio 164a
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+if ( ! class_exists( 'Charitable_Admin_Donation_Form' ) ) :
+
+	/**
+	 * Charitable_Admin_Donation_Form
+	 *
+	 * @since  1.5.0
+	 */
+	class Charitable_Admin_Donation_Form extends Charitable_Donation_Form implements Charitable_Donation_Form_Interface {
+
+		/**
+		 * Campaign object. May be null.
+		 *
+		 * @since 1.5.0
+		 *
+		 * @var   Charitable_Campaign
+		 */
+		protected $campaign;
+
+		/**
+		 * Set of form fields.
+		 *
+		 * @since 1.5.0
+		 *
+		 * @var   array
+		 */
+		protected $form_fields;
+
+		/**
+		 * Nonce action.
+		 *
+		 * @since 1.5.0
+		 *
+		 * @var   string
+		 */
+		protected $nonce_action = 'charitable_admin_donation_form';
+
+		/**
+		 * Nonce name.
+		 *
+		 * @since 1.5.0
+		 *
+		 * @var   string
+		 */
+		protected $nonce_name = '_charitable_admin_donation_form_nonce';
+
+		/**
+		 * Action to be executed upon form submission.
+		 *
+		 * @since 1.5.0
+		 *
+		 * @var   string
+		 */
+		protected $form_action = 'save_donation_admin';
+
+		/**
+		 * Create a donation form object.
+		 *
+		 * @since 1.5.0
+		 *
+		 * @param Charitable_Campaign|null $campaign Optional. Campaign receiving the donation, or NULL if 
+		 *                                           the campaign will be selected in the form.
+		 */
+		public function __construct( Charitable_Campaign $campaign = null ) {
+			$this->campaign = $campaign;
+			$this->id       = uniqid();
+
+			$this->attach_hooks_and_filters();
+		}
+
+		/**
+		 * Return the donation form fields.
+		 *
+		 * @since  1.5.0
+		 *
+		 * @return array[]
+		 */
+		public function get_fields() {
+			$fields = apply_filters( 'charitable_admin_donation_form_fields', array(
+				'donation_fields' => array(
+					'legend'   => __( 'Donation', 'charitable' ),
+					'type'     => 'charitable-fieldset',
+					'fields'   => $this->get_donation_fields(),
+					'priority' => 20,
+				),
+				'user_fields' => array(
+					'legend'   => __( 'Donor Details', 'charitable' ),
+					'type'     => 'fieldset',
+					'fields'   => $this->get_user_fields(),
+					'class'    => 'charitable-fieldset',
+					'priority' => 40,
+				),
+			), $this );
+
+			uasort( $fields, 'charitable_priority_sort' );
+
+			return $fields;
+		}
+
+		/**
+		 * Get donation fields.
+		 *
+		 * @since  1.5.0
+		 *
+		 * @return array
+		 */
+		public function get_donation_fields() {
+			return array();
+		}
+
+		/**
+		 * Validate the form submission.
+		 *
+		 * @since  1.4.4
+		 *
+		 * @return boolean
+		 */
+		public function validate_submission() {
+
+			/* If we have already validated the submission, return the value. */
+			if ( $this->validated ) {
+				return $this->valid;
+			}
+
+			$this->validated = true;
+
+			$this->valid = $this->validate_security_check()
+				&& $this->check_required_fields( $this->get_merged_fields() )
+				&& $this->validate_amount();
+
+			$this->valid = apply_filters( 'charitable_validate_admin_donation_form_submission', $this->valid, $this );
+
+			return $this->valid;
+
+		}
+
+		/**
+		 * Return the donation values.
+		 *
+		 * @since  1.5.0
+		 *
+		 * @return array
+		 */
+		public function get_donation_values() {
+			$submitted = $this->get_submitted_values();
+
+
+			return apply_filters( 'charitable_admin_donation_form_submission_values', $values, $submitted, $this );
+		}
+
+		/**
+		 * Render the donation form.
+		 *
+		 * @since  1.5.0
+		 *
+		 * @return void
+		 */
+		public function render() {
+			charitable_template( 'donation-form/form-donation.php', array(
+				'campaign' => null,
+				'form'     => $this,
+				'form_id'  => 'charitable-admin-donation-form',
+			) );
+		}
+
+		/**
+		 * Render a form field.
+		 *
+		 * @since  1.5.0
+		 *
+		 * @param  array           $field     Field definition.
+		 * @param  string          $key       Field key.
+		 * @param  Charitable_Form $form      The form object.
+		 * @param  int             $index     The current index.
+		 * @param  string          $namespace Namespace for the form field's name attribute.
+		 * @return boolean False if the field was not rendered. True otherwise.
+		 */
+		public function render_field( $field, $key, $form, $index = 0, $namespace = null ) {
+			if ( ! $form->is_current_form( $this->id ) ) {
+				return false;
+			}
+
+			if ( ! isset( $field['type'] ) ) {
+				return false;
+			}
+
+			
+		}
+	}
+
+endif;
--- templates/donation-form/donation-amount.php
+++ templates/donation-form/donation-amount.php
@@ -14,11 +14,14 @@ if ( ! isset( $view_args['form'] ) ) {
 	return;
 }
 
-/**
- * @var Charitable_Donation_Form
- */
-$form            = $view_args['form'];
-$campaign        = $form->get_campaign();
+/* @var Charitable_Donation_Form */
+$form     = $view_args['form'];
+$campaign = $form->get_campaign();
+
+if ( is_null( $campaign ) ) {
+	return;
+}
+
 $suggested       = $campaign->get_suggested_donations();
 $amount          = $campaign->get_donation_amount_in_session();
 $currency_helper = charitable_get_currency_helper();
@@ -41,88 +44,9 @@ do_action( 'charitable_donation_form_before_donation_amount', $view_args['form']
 	 * @hook    charitable_donation_form_before_donation_amounts
 	 */
 	do_action( 'charitable_donation_form_before_donation_amounts', $view_args['form'] );
-	?>
 
-
-<?php if ( count( $suggested ) ) :
-
-	$amount_is_suggestion = false; ?>
-
-	<ul class="donation-amounts">
-
-		<?php foreach ( $suggested as $suggestion ) :
-
-			$checked  = checked( $suggestion['amount'], $amount, false );
-			$field_id = esc_attr( sprintf( 'form-%s-field-%s',
-				$view_args['form']->get_form_identifier(),
-				$suggestion['amount']
-			) );
-
-			if ( strlen( $checked ) ) :
-
-				$amount_is_suggestion = true;
-
-			endif; ?>
-
-			<li class="donation-amount suggested-donation-amount">
-				<label for="<?php echo $field_id ?>">
-					<input
-						id="<?php echo $field_id ?>"
-						type="radio"
-						name="donation_amount"
-						value="<?php echo esc_attr( $currency_helper->sanitize_database_amount( $suggestion['amount'] ) ) ?>" <?php echo $checked ?>
-					/><?php printf(
-						'<span class="amount">%s</span> <span class="description">%s</span>',
-						charitable_format_money( $suggestion['amount'], false, true ),
-						isset( $suggestion['description'] ) ? $suggestion['description'] : ''
-					) ?>
-				</label>
-			</li>
-
-		<?php endforeach; ?>
-
-		<?php if ( $campaign->get( 'allow_custom_donations' ) ) :
-
-			$has_custom_donation_amount = ! $amount_is_suggestion && $amount; ?>
-
-			<li class="donation-amount custom-donation-amount">
-				<span class="custom-donation-amount-wrapper">
-					<label for="form-<?php echo esc_attr( $view_args['form']->get_form_identifier() ) ?>-field-custom-amount">
-						<input
-							id="form-<?php echo esc_attr( $view_args['form']->get_form_identifier() ) ?>-field-custom-amount"
-							type="radio"
-							name="donation_amount"
-							value="custom" <?php checked( $has_custom_donation_amount ) ?>
-						/><span class="description"><?php echo apply_filters( 'charitable_donation_amount_form_custom_amount_text', __( 'Custom amount', 'charitable' ) ) ?></span>
-					</label>
-					<input
-						type="text"
-						class="custom-donation-input"
-						name="custom_donation_amount"
-						value="<?php if ( $has_custom_donation_amount ) { echo $amount; } ?>" 
-					/>
-				</span>
-			</li>
-
-		<?php endif ?>
-
-	</ul>
-
-<?php elseif ( $campaign->get( 'allow_custom_donations' ) ) : ?>
-
-	<div id="custom-donation-amount-field" class="charitable-form-field charitable-custom-donation-field-alone">
-		<input
-			type="text"
-			class="custom-donation-input"
-			name="custom_donation_amount"
-			placeholder="<?php esc_attr_e( 'Enter donation amount', 'charitable' ) ?>"
-			value="<?php if ( $amount ) { echo esc_attr( $amount ); } ?>" 
-		/>
-	</div>
-
-<?php endif ?>
-
-	<?php
+	charitable_template( 'donation-form/donation-amount-list.php', array( 'campaign' => $campaign, 'form' => $form ) );
+	
 	/**
 	 * @hook    charitable_donation_form_after_donation_amounts
 	 */
