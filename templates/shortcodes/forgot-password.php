19024954059efd84cd69c0b4ac77f617f6d520f4
ericnicolaas <eric@ericnicolaas.com>
2017-12-07 17:18:25 +0930

    Code consistency changes for templates/shortcodes/forgot-password.php

--- templates/shortcodes/forgot-password.php
+++ templates/shortcodes/forgot-password.php
@@ -17,37 +17,37 @@ $form = $view_args['form'];
 <div class="charitable-forgot-password-form">
 	<?php
 	/**
-	* @hook charitable_forgot_password_before
-	* @param   array $view_args Shortcode attributes.
-	*/
+	 * @hook charitable_forgot_password_before
+	 * @param   array $view_args Shortcode attributes.
+	 */
 	do_action( 'charitable_forgot_password_before', $view_args );
 
 	?>
 	<form id="lostpasswordform" class="charitable-form" action="<?php echo wp_lostpassword_url() ?>" method="post">
 
-		<?php 
+		<?php
 		/**
 		 * @hook charitable_form_before_fields
 		 * @param  obj $form
 		 * @param  array $view_args All args passed to template.
 		 */
-		do_action( 'charitable_form_before_fields', $form, $view_args ) 
+		do_action( 'charitable_form_before_fields', $form, $view_args )
 		?>
-		
+
 
 		<div class="charitable-form-fields cf">
 			<?php $form->view()->render() ?>
 		</div><!-- .charitable-form-fields -->
 
-		<?php 
+		<?php
 		/**
 		 * @hook charitable_form_before_fields
 		 * @param  obj $form
 		 * @param  array $view_args All args passed to template.
 		 */
-		do_action( 'charitable_form_after_fields', $form, $view_args ); 
+		do_action( 'charitable_form_after_fields', $form, $view_args );
 		 ?>
-	
+
 		<div class="charitable-form-field charitable-submit-field">
 			<input type="submit" name="submit" class="lostpassword-button" value="<?php esc_attr_e( 'Reset Password', 'charitable' ) ?>" />
 		</div>
@@ -55,9 +55,9 @@ $form = $view_args['form'];
 	</form><!-- #lostpasswordform -->
 	<?php
 	/**
-	* @hook charitable_forgot_password_after
-	* @param   array $view_args Shortcode attributes.
-	*/
+	 * @hook charitable_forgot_password_after
+	 * @param   array $view_args Shortcode attributes.
+	 */
 	do_action( 'charitable_forgot_password_after', $view_args );
 	?>
 </div>
