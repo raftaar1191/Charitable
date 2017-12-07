1335f327696221904ecf79b729be4c0fac11a90b
ericnicolaas <eric@ericnicolaas.com>
2017-12-07 17:18:17 +0930

    Code consistency changes for includes/shortcodes/class-charitable-donation-form-shortcode.php

--- includes/shortcodes/class-charitable-donation-form-shortcode.php
+++ includes/shortcodes/class-charitable-donation-form-shortcode.php
@@ -34,7 +34,7 @@ if ( ! class_exists( 'Charitable_Donation_Form_Shortcode' ) ) :
 			$defaults = array (
 		 		'campaign_id' => 0
 			);
-	
+
 			// Parse incoming $atts into an array and merge it with $defaults
 			$args = shortcode_atts( $defaults, $atts, 'charitable_donation_form' );
 
@@ -48,18 +48,18 @@ if ( ! class_exists( 'Charitable_Donation_Form_Shortcode' ) ) :
 		    if ( ! wp_script_is( 'charitable-script', 'enqueued' ) ) {
 		        Charitable_Public::get_instance()->enqueue_donation_form_scripts();
 		    }
-		    
+
 		    $form = charitable_get_campaign( $args['campaign_id'] )->get_donation_form();
 
 		    do_action( 'charitable_donation_form_before', $form );
 
 		    $args['form'] = $form;
 		    $args['campaign'] = $form->get_campaign();
-		    
+
 		    charitable_template( 'donation-form/form-donation.php', $args );
-		    
+
 		    do_action( 'charitable_donation_form_after', $form );
-		    
+
 		    return ob_get_clean();
 
 		}
