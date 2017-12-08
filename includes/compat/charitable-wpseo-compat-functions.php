6027903a364d97603cd321d30c093ff4c123e01e
Eric Daams <eric@164a.com>
2017-12-08 20:32:50 +0930

    Check pagenow before loading template hooks/functions in admin. Closes #488.

--- includes/compat/charitable-wpseo-compat-functions.php
+++ includes/compat/charitable-wpseo-compat-functions.php
@@ -21,8 +21,12 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
  * @return void
  */
 function charitable_wpseo_compat_load_template_files() {
-    require_once( charitable()->get_path( 'includes' ) . 'public/charitable-template-functions.php' );
-    require_once( charitable()->get_path( 'includes' ) . 'public/charitable-template-hooks.php' );
+    global $pagenow;
+
+    if ( in_array( $pagenow, array( 'post.php', 'edit.php', 'post-new.php' ) ) ) {
+        require_once( charitable()->get_path( 'includes' ) . 'public/charitable-template-functions.php' );
+        require_once( charitable()->get_path( 'includes' ) . 'public/charitable-template-hooks.php' );
+    }    
 }
 
 add_action( 'admin_init', 'charitable_wpseo_compat_load_template_files' );
