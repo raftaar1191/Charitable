9653d8abdd0fda1595161a491b207fa24150c86c
Eric Daams <eric@164a.com>
2018-02-19 10:34:47 +0930

    Added donation form icon?

---
+++ src/blocks/donation-form/icon.js
@@ -0,0 +1,7 @@
+const icon = <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 20 20">
+    <path d="M17.69,2.31V5.69H2.31V2.31H17.69M20,0H0V8H20V0Z"/>
+    <path d="M17.69,8.31v3.38H2.31V8.31H17.69M20,6H0v8H20V6Z"/>
+    <path d="M17.69,14.31v3.38H2.31V14.31H17.69M20,12H0v8H20V12Z"/>
+</svg>;
+
+export default icon;
