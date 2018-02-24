d8eae973eddf600e459deee8d241629bba68727f
Eric Daams <eric@164a.com>
2018-02-24 23:36:43 +0930

    Early pass at setting up a single Campaign Settings meta box, but this is basically working as a foundation to build on.

--- includes/admin/forms/views/class-charitable-admin-form-view.php
+++ includes/admin/forms/views/class-charitable-admin-form-view.php
@@ -182,6 +182,8 @@ if ( ! class_exists( 'Charitable_Admin_Form_View' ) ) :
 
 			/* Increment the tabindex. */
 			$this->tabindex = $field['tabindex'] + 1;
+			
+			error_log( var_export( $field, true ) );
 
 			return charitable_admin_view( $field['view'], $field );
 		}
