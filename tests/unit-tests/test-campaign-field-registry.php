1c630864ea59038337bb75260e3ca38612b6e148
Eric Daams <eric@164a.com>
2018-03-13 11:28:27 +0930

    Added tests to check on the value callbacks via field registries.

--- tests/unit-tests/test-campaign-field-registry.php
+++ tests/unit-tests/test-campaign-field-registry.php
@@ -48,4 +48,30 @@ class Test_Charitable_Campaign_Field_Registry extends Charitable_UnitTestCase {
         $registry->register_field( new Charitable_Campaign_Field( 'field' ) );
         $this->assertInstanceOf( 'Charitable_Campaign_Field', $registry->get_field( 'field' ) );
     }
+
+    /**
+     * @depends test_register_field
+     * @covers Charitable_Field_Registry::get_field
+     * @covers Charitable_Campaign_Field_Registry::get_field_value_callback
+     */
+    public function test_get_field_value_callback_without_callback() {
+        $registry = new Charitable_Campaign_Field_Registry();
+        $field    = new Charitable_Campaign_Field( 'field' );
+        $registry->register_field( $field );
+        $this->assertFalse( $registry->get_field_value_callback( $field ) );
+    }
+
+    /**
+     * @depends test_register_field
+     * @covers Charitable_Field_Registry::get_field
+     * @covers Charitable_Campaign_Field_Registry::get_field_value_callback
+     */
+    public function test_get_field_value_callback_with_callback() {
+        $registry = new Charitable_Campaign_Field_Registry();
+        $field    = new Charitable_Campaign_Field( 'field', array(
+            'value_callback' => '__return_true',
+        ) );
+        $registry->register_field( $field );
+        $this->assertEquals( '__return_true', $registry->get_field_value_callback( $field ) );
+    }
 }
