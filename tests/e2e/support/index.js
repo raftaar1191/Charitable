9d924dd8a7f9d333c98eb514efc6da0f5428952f
Eric Daams <eric@164a.com>
2018-03-15 19:48:59 +0930

    Added e2e testing framework, with initial set of tests focused on campaign creation.

---
+++ tests/e2e/support/index.js
@@ -0,0 +1,30 @@
+Cypress.Commands.add( 'newCampaign', () => {
+	cy.visitAdmin( '/post-new.php?post_type=campaign' );
+} );
+
+Cypress.Commands.add( 'visitAdmin', ( adminPath ) => {
+	cy.visit( '/wp-admin/' + adminPath ).location( 'pathname' ).then( ( path ) => {
+		if ( path.endsWith( '/wp-login.php' ) ) {
+			cy.login();
+		}
+	} );
+} );
+
+Cypress.Commands.add( 'login', ( username = Cypress.env( 'username' ), password = Cypress.env( 'password' ) ) => {
+	// A best practice would be to avoid this in each test
+	// and fake it by calling an API and setting a cookie
+	// (not sure this is possible in WP)
+
+	cy.location( 'pathname' ).then( ( path ) => {
+		if ( ! path.endsWith( '/wp-login.php' ) ) {
+			cy.visit( '/wp-login.php' );
+		}
+	} );
+	cy.get( '#user_login' ).type( username );
+	cy.get( '#user_pass' ).type( password );
+	cy.get( '#wp-submit' ).click();
+} );
+
+Cypress.Cookies.defaults( {
+	whitelist: /^wordpress_/,
+} );
