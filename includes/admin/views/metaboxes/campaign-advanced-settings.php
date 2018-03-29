66a7cbdcbb6b0c514a18cddde6f89961041f0f84
Eric Daams <eric@164a.com>
2018-03-29 16:58:31 +0930

    Tidied up formatting

--- includes/admin/views/metaboxes/campaign-advanced-settings.php
+++ includes/admin/views/metaboxes/campaign-advanced-settings.php
@@ -2,10 +2,11 @@
 /**
  * Loops over the meta boxes inside the advanced settings area of the Campaign post type.
  *
- * @author 		Studio 164a
- * @package     Charitable/Admin Views/Metaboxes
- * @copyright 	Copyright (c) 2018, Studio 164a
- * @since 		1.0.0
+ * @author    Studio 164a
+ * @package   Charitable/Admin Views/Metaboxes
+ * @copyright Copyright (c) 2018, Studio 164a
+ * @since     1.0.0
+ * @version   1.6.0
  */
 
 global $post;
@@ -19,13 +20,13 @@ if ( ! isset( $view_args['meta_boxes'] ) || empty( $view_args['meta_boxes'] ) ) 
 <div id="charitable-campaign-advanced-metabox" class="charitable-metabox">
 	<ul class="charitable-tabs">
 		<?php foreach ( $view_args['meta_boxes'] as $meta_box ) : ?>
-			<li><a href="<?php printf( '#%s', $meta_box['id'] ) ?>"><?php echo $meta_box['title'] ?></a></li>
+			<li><a href="<?php printf( '#%s', $meta_box['id'] ); ?>"><?php echo $meta_box['title']; ?></a></li>
 		<?php endforeach ?>
 	</ul>
 	<?php foreach ( $view_args['meta_boxes'] as $meta_box ) : ?>
-		<div id="<?php echo $meta_box['id'] ?>" class="postbox <?php echo postbox_classes( $meta_box['id'], 'campaign' ) ?>">
+		<div id="<?php echo $meta_box['id']; ?>" class="postbox <?php echo postbox_classes( $meta_box['id'], 'campaign' ); ?>">
 			<div class="inside">
-				<?php call_user_func( $meta_box['callback'], $post, $meta_box ) ?>
+				<?php call_user_func( $meta_box['callback'], $post, $meta_box ); ?>
 			</div>
 		</div>
 	<?php
