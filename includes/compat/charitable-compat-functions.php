d11a7ca0cc4a1d22b08593c073d0824448638de1
Eric Daams <eric@164a.com>
2018-04-06 14:16:24 +0930

    Registered /reports/ API route. See #543. Closes #542.

--- includes/compat/charitable-compat-functions.php
+++ includes/compat/charitable-compat-functions.php
@@ -20,30 +20,30 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
  * @return void
  */
 function charitable_load_compat_functions() {
-    $includes_path = charitable()->get_path( 'includes' );
+	$includes_path = charitable()->get_path( 'includes' );
 
-    /* WP Super Cache */
-    if ( function_exists( 'wp_super_cache_text_domain' ) ) {
-        require_once( $includes_path . 'compat/charitable-wp-super-cache-compat-functions.php' );
-    }
+	/* WP Super Cache */
+	if ( function_exists( 'wp_super_cache_text_domain' ) ) {
+		require_once( $includes_path . 'compat/charitable-wp-super-cache-compat-functions.php' );
+	}
 
-    /* W3TC */
-    if ( defined( 'W3TC' ) && W3TC ) {
-        require_once( $includes_path . 'compat/charitable-w3tc-compat-functions.php' );
-    }
+	/* W3TC */
+	if ( defined( 'W3TC' ) && W3TC ) {
+		require_once( $includes_path . 'compat/charitable-w3tc-compat-functions.php' );
+	}
 
-    /* WP Rocket */
-    if ( defined( 'WP_ROCKET_VERSION' )  ) {
-        require_once( $includes_path . 'compat/charitable-wp-rocket-compat-functions.php' );
-    }
+	/* WP Rocket */
+	if ( defined( 'WP_ROCKET_VERSION' )  ) {
+		require_once( $includes_path . 'compat/charitable-wp-rocket-compat-functions.php' );
+	}
 
-    /* WP Fastest Cache */
-    if ( class_exists( 'WpFastestCache' ) ) {
-        require_once( $includes_path . 'compat/charitable-wp-fastest-cache-compat-functions.php' );
-    }
+	/* WP Fastest Cache */
+	if ( class_exists( 'WpFastestCache' ) ) {
+		require_once( $includes_path . 'compat/charitable-wp-fastest-cache-compat-functions.php' );
+	}
 
-    /* Yoast SEO */
-    if ( defined( 'WPSEO_VERSION' ) ) {
-        require_once( $includes_path . 'compat/charitable-wpseo-compat-functions.php' );
-    }
+	/* Yoast SEO */
+	if ( defined( 'WPSEO_VERSION' ) ) {
+		require_once( $includes_path . 'compat/charitable-wpseo-compat-functions.php' );
+	}
 }
