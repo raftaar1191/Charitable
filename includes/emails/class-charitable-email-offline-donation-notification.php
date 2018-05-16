503296c7cd0d24582d58f901146ea7ed045c325e
Eric Daams <eric@164a.com>
2018-05-16 09:59:04 +0930

    Added filters to can_be_resent() method for email classes. Closes #575.

--- includes/emails/class-charitable-email-offline-donation-notification.php
+++ includes/emails/class-charitable-email-offline-donation-notification.php
@@ -155,7 +155,19 @@ if ( ! class_exists( 'Charitable_Email_Offline_Donation_Notification' ) && class
 		 * @return boolean
 		 */
 		public static function can_be_resent( $object_id, $args = array() ) {
-			return 'charitable-pending' == get_post_status( $object_id ) && 'offline' == get_post_meta( $object_id, 'donation_gateway', true );
+			$resendable = 'charitable-pending' == get_post_status( $object_id )
+				&& 'offline' == get_post_meta( $object_id, 'donation_gateway', true );
+
+			/**
+			 * Filter whether the email can be resent.
+			 *
+			 * @since 1.6.0
+			 *
+			 * @param boolean $resendable Whether the email can be resent.
+			 * @param int     $object_id  The donation ID.
+			 * @param array   $args       Mixed set of arguments.
+			 */
+			return apply_filters( 'charitable_can_resend_offline_donation_notification_email', $resendable, $object_id, $args );
 		}
 
 
