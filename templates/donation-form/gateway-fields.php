fa3539d892541d4b00619db12931d91529b49e51
Eric Daams <eric@164a.com>
2019-05-31 16:23:15 +0930

    Tidied up formatting of template file. Also added comment about how to override.

--- templates/donation-form/gateway-fields.php
+++ templates/donation-form/gateway-fields.php
@@ -2,6 +2,8 @@
 /**
  * The template used to display the gateway fields.
  *
+ * Override this template by copying it to yourtheme/charitable/donation-form/gateway-fields.php
+ *
  * @author  Studio 164a
  * @package Charitable/Templates/Donation Form
  * @since   1.0.0
@@ -9,7 +11,9 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) { exit; }
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 if ( ! isset( $view_args['form'] ) || ! isset( $view_args['field'] ) ) {
 	return;
@@ -24,42 +28,40 @@ $default  = isset( $field['default'] ) && isset( $gateways[ $field['default'] ] 
 ?>
 <fieldset id="charitable-gateway-fields" class="charitable-fieldset">
 	<?php
-	if ( isset( $field['legend'] ) ) : ?>
-
-		<div class="charitable-form-header"><?php echo $field['legend'] ?></div>
-
-	<?php
+	if ( isset( $field['legend'] ) ) :
+		?>
+		<div class="charitable-form-header"><?php echo $field['legend']; ?></div>
+		<?php
 	endif;
 
 	if ( count( $gateways ) > 1 ) :
-	?>
+		?>
 		<fieldset class="charitable-fieldset-field-wrapper">
 			<div class="charitable-fieldset-field-header" id="charitable-gateway-selector-header"><?php _e( 'Choose Your Payment Method', 'charitable' ) ?></div>
 			<ul id="charitable-gateway-selector" class="charitable-radio-list charitable-form-field">
 				<?php foreach ( $gateways as $gateway_id => $details ) : ?>
-					<li><input type="radio" 
-							id="gateway-<?php echo esc_attr( $gateway_id ) ?>"
+					<li><input type="radio"
+							id="gateway-<?php echo esc_attr( $gateway_id ); ?>"
 							name="gateway"
-							value="<?php echo esc_attr( $gateway_id ) ?>"
+							value="<?php echo esc_attr( $gateway_id ); ?>"
 							aria-describedby="charitable-gateway-selector-header"
-							<?php checked( $default, $gateway_id ) ?> />
-						<label for="gateway-<?php echo esc_attr( $gateway_id ) ?>"><?php echo $details['label'] ?></label>
+							<?php checked( $default, $gateway_id ); ?> />
+						<label for="gateway-<?php echo esc_attr( $gateway_id ); ?>"><?php echo $details['label']; ?></label>
 					</li>
 				<?php endforeach ?>
 			</ul>
 		</fieldset>
-	<?php
+		<?php
 	endif;
 
 	foreach ( $gateways as $gateway_id => $details ) :
-
 		if ( ! isset( $details['fields'] ) || empty( $details['fields'] ) ) :
 			continue;
 		endif;
 
 		?>
-		<div id="charitable-gateway-fields-<?php echo $gateway_id ?>" class="charitable-gateway-fields charitable-form-fields cf" data-gateway="<?php echo $gateway_id ?>">
-			<?php $form->view()->render_fields( $details['fields'] ) ?>
-		</div><!-- #charitable-gateway-fields-<?php echo $gateway_id ?> -->	
+		<div id="charitable-gateway-fields-<?php echo $gateway_id; ?>" class="charitable-gateway-fields charitable-form-fields cf" data-gateway="<?php echo $gateway_id ?>">
+			<?php $form->view()->render_fields( $details['fields'] ); ?>
+		</div><!-- #charitable-gateway-fields-<?php echo $gateway_id; ?> -->
 	<?php endforeach ?>
 </fieldset><!-- .charitable-fieldset -->
