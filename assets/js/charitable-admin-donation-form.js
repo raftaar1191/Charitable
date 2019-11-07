2f70f2210fdf392c7ca348875d64e17c74c2c393
Eric Daams <eric@164a.com>
2019-11-07 19:16:23 +0930

    Got rid of empty js files

--- assets/js/charitable-admin-donation-form.js
+++
@@ -1,9 +0,0 @@
-CHARITABLE_ADMIN = window.CHARITABLE_ADMIN || {};
-
-( function( exports, $ ) {
-	var Admin_Donation_Form = function( $el ) {
-		console.log( 'hello' );
-	};
-
-	exports.Admin_Donation_Form = Admin_Donation_Form;
-} )( CHARITABLE_ADMIN, jQuery );
